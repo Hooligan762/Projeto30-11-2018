477241611c9eb201db549d44ca8f5544130f83f7
Hooligan762 <hooligan7621@gmail.com>
2018-11-30 21:14:30 -0200

    Realizado com sucesso!

---
+++ src/projeto30/pkg11/pkg2018/Aluno.java
@@ -0,0 +1,69 @@
+
+package projeto30.pkg11.pkg2018;
+
+import java.util.Objects;
+
+
+public class Aluno {
+    private String nome;
+    private Integer idade;
+
+    public Aluno(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "nome=" + nome + " idade=" + idade + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.nome);
+        hash = 71 * hash + Objects.hashCode(this.idade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aluno other = (Aluno) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.idade, other.idade)) {
+            return false;
+        }
+        return true;
+    }
+
+   
+    
+    
+}
--- src/projeto30/pkg11/pkg2018/Projeto30112018.java
+++ src/projeto30/pkg11/pkg2018/Projeto30112018.java
@@ -5,6 +5,12 @@
  */
 package projeto30.pkg11.pkg2018;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *
  * @author Aluno
@@ -15,7 +21,38 @@ public class Projeto30112018 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       Aluno a1 = new Aluno("Maria", 31);
+       Aluno a2 = new Aluno("Maria", 31);
+       Aluno a3 = new Aluno("Maria", 31);
+       
+        List<Aluno> listaAluno = new ArrayList<>();
+////        listaAluno.add(a1);
+////        listaAluno.add(a2);
+////        listaAluno.add(a3);
+//       
+//           listaAluno.addAll(Arrays.asList(a1,a2,a3));
+//           
+//           System.out.println(a3 + "\n" + a2 );
+           
+           for (Aluno listaAluno1 : listaAluno) {
+           System.out.println(listaAluno1.getNome() + "\n" + listaAluno1.getIdade());
+               
+            
+        }
+           Set<Aluno> listaAlunoSet = new HashSet<>();
+           listaAlunoSet.add(a1);
+           listaAlunoSet.add(a2);
+           listaAlunoSet.add(a3);
+           
+           for ( Aluno listaAlunoSet1 : listaAlunoSet) {
+               System.out.println(listaAlunoSet1.getNome());
+           }
+           
+           System.out.println(a1.equals(a3));
+        }
+            
+        }
+        
+
     
-}
+
